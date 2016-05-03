5fbed6fbc72468da94db3e8851cf5b3ac085d705
Gustavo Puma <vash47@gmail.com>
2016-05-02 21:50:40 -0500

    lab 3 done

--- mongomart/items.js
+++ mongomart/items.js
@@ -209,15 +209,9 @@ function ItemDAO(database) {
          * _id and pass the matching item to the callback function.
          *
          */
-
-        var item = this.createDummyItem();
-
-        // TODO-lab3 Replace all code above (in this method).
-
-        // TODO Include the following line in the appropriate
-        // place within your code to pass the matching item
-        // to the callback.
-        callback(item);
+        this.db.collection('item').findOne({_id: itemId}, function(err, item){
+            callback(item);
+        });
     }
 
 
